150511e5e99eaac99b23be700373647b0b6a4a1c
Derrick Chea <derrick.c.chea@gmail.com>
2018-09-12 22:30:44 -0400

    Add files via upload

--- Election.sol
+++ Election.sol
@@ -1,11 +1,26 @@
 pragma solidity ^0.4.12;
 
 contract Election {
-    // Stor canidate 
-    // Read candidate
-    string public candidate;
-    // construtor
+    // Model a candidate
+    // Store Candidates
+    // Fetch Candidate
+    // Store Candidates Count
+
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+    mapping(uint => Candidate) public candidates;
+
+    uint public candidatesCount;
+
     function Election () public {
-        candidate = "candidate 1";
+
+    }
+
+    function addCandidate (string_name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
