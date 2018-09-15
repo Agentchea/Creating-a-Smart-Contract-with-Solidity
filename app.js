f5dfc0ba9ac61d7460d3e03dedff2f1f25da7d2b
Derrick Chea <derrick.c.chea@gmail.com>
2018-09-15 16:20:29 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,80 @@
+App = {
+  web3Provider: null,
+  contracts: {},
+
+  init: function() {
+    // Load pets.
+    $.getJSON('../pets.json', function(data) {
+      var petsRow = $('#petsRow');
+      var petTemplate = $('#petTemplate');
+
+      for (i = 0; i < data.length; i ++) {
+        petTemplate.find('.panel-title').text(data[i].name);
+        petTemplate.find('img').attr('src', data[i].picture);
+        petTemplate.find('.pet-breed').text(data[i].breed);
+        petTemplate.find('.pet-age').text(data[i].age);
+        petTemplate.find('.pet-location').text(data[i].location);
+        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
+
+        petsRow.append(petTemplate.html());
+      }
+    });
+
+    return App.initWeb3();
+  },
+
+  initWeb3: function() {
+    // TODO: refactor conditional
+    if (typeof web3 !== 'undefined') {
+      // if a web3 instance is already provided by Meta Mask.
+      App.web3provider = web3.currebtProvider;
+      web3 = new WebAuthentication(web3.currentProvider);
+    } else {
+      // Specify default instance if no web3 instance provided
+      app.web3Provider = new Web3.providers.httpProvider('http://localhost:7545');
+      web3 = new Web3(App.web3Provider);
+    }
+
+    return App.initContract();
+  },
+
+  initContract: function() {
+    $.getJSON("Election.json"), function(election) {
+      //instantiate a new truffle contract from the artifact
+      App.contracts.Election = TruffleContract(election);
+      // conmnect provider to interact with conbtract
+      App/contracts.Election.setProvider(App.web3Provider);
+
+      return App.render();
+    }
+
+    return App.bindEvents();
+  },
+
+  bindEvents: function() {
+    $(document).on('click', '.btn-adopt', App.handleAdopt);
+  },
+
+  markAdopted: function(adopters, account) {
+    /*
+     * Replace me...
+     */
+  },
+
+  handleAdopt: function(event) {
+    event.preventDefault();
+
+    var petId = parseInt($(event.target).data('id'));
+
+    /*
+     * Replace me...
+     */
+  }
+
+};
+
+$(function() {
+  $(window).load(function() {
+    App.init();
+  });
+});
